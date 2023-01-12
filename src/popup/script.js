c697318fc8a6f24a346ed81f400aa374ed27445b
Jesper Olsson <c.jesper.olsson@gmail.com>
2023-01-12 08:11:46 +0100

    Simplifying some things

--- src/popup/script.js
+++ src/popup/script.js
@@ -8,50 +8,44 @@ document.addEventListener('DOMContentLoaded', function() {
 	    let tabIdStr = tab.id.toString();	
 
 		// Get input fields.
-		let input = document.getElementById('interface').children;
-		// Add input change event. The 'input' event would allow us to update number field whilst dragging
-		for (let i = 0; i < input.length; i++) {
-			input[i].addEventListener('change', function() {
-				// Set value to other input field.
-				for (let j = 0; j < input.length; j++) {
-					if (input[j] === this) {
-						continue;
-					}
-					input[j].value = this.value;
-				}
-				
-				// Set volume level of tab.
-				platform.runtime.sendMessage({ id: tab.id, volume: this.value });
-				// Store value level.
-				platform.storage.local.set({ [tabIdStr]: this.value });
-			});
-		}
-		// Add button click event.
-		document.getElementById('stop').addEventListener('click', function() {
+		let buttonStop = document.getElementById('stop');
+		let rangeVolume = document.getElementById('range');
+		let numberVolume = document.getElementById('number');
+		
+		buttonStop.addEventListener('click', function() {
 			// Set volume to default 100 to disable the system.
 			platform.runtime.sendMessage({ id: tab.id, volume: 100 });
-			// Remove value level
 			platform.storage.local.remove(tabIdStr);
-			// Exit the window.
 			window.close();
 		});
+
+		function setVolume(vol) {
+			platform.runtime.sendMessage({ id: tab.id, volume: vol });
+			platform.storage.local.set({ [tabIdStr]: vol });	
+		}
 		
-		// Get volume level from storage.
+		rangeVolume.addEventListener('change', function() {
+			numberVolume.value = this.value;
+			setVolume(this.value);
+		});
+		
+		rangeVolume.addEventListener('input', function() {
+			numberVolume.value = this.value;
+		});
+		
+		numberVolume.addEventListener('change', function() {
+			rangeVolume.value = this.value;
+			setVolume(this.value);
+		});
+		
+		
+		// Get volume level from storage
 		platform.storage.local.get(tabIdStr, function(items) {
-			// Apply volume level.
-			let volume = items[tabIdStr];
-			if (volume) {
+			let volume = items[tabIdStr] ?? 100;
+			if (volume != 100) {
 				platform.runtime.sendMessage({ id: tab.id, volume: volume });
 			}
-			// If no volume level given set default of 100.
-			else {
-				volume = 100;
-			}
-			
-			// Apply volume to interface.
-			for (let k = 0; k < input.length; k++) {
-				input[k].value = volume;
-			}
+			rangeVolume.value = numberVolume.value = volume;
 		});
 	});
 });
